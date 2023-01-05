b2475df0f8ed4fd505ed7ee1c8d315e205791b75
ABHI <ABHI@DESKTOP-2B0L59T>
2023-01-05 21:00:43 +0530

    simple hello project

---
+++ DemoSimplePro/src/com/info/docker/demo/simple/Hello.java
@@ -0,0 +1,10 @@
+package com.info.docker.demo.simple;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
